db5bd796777b4e6e90657f9c4fe52a280b51602f
Umar-Shaikh99 <118403393+Umar-Shaikh99@users.noreply.github.com>
2024-07-23 19:48:02 +0530

    Add files via upload

---
+++ actions/auth-actions.js
@@ -0,0 +1,80 @@
+"use server";
+
+import { createAuthSession, destroySession } from "@/lib/auth";
+import { hashUserPassword, verifyPassword } from "@/lib/hash";
+import { createUser, getUserByEmail } from "@/lib/user";
+import { redirect } from "next/navigation";
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter the valid email address";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "The password must be an 8 character long";
+  }
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+
+  //This is the hashed password we hashed the password using nodejs functionality and save into our db
+  const hashedPassword = hashUserPassword(password);
+  try {
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect("/training");
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email: "Looks Like Account Is Already Exist",
+        },
+      };
+    }
+    throw error;
+  }
+}
+
+export async function login(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const existingUser = getUserByEmail(email);
+
+  if (!existingUser) {
+    return {
+      errors: {
+        email: "Could not authenticate user, please check your credentials",
+      },
+    };
+  }
+  const isValidPassword = verifyPassword(existingUser.password, password);
+  if (!isValidPassword) {
+    return {
+      errors: {
+        password: "Could not authenticate user, please check your credentials",
+      },
+    };
+  }
+  await createAuthSession(existingUser.id);
+  redirect("/training");
+}
+
+export async function auth(mode, prevState, formData) {
+  if (mode === "login") {
+    return login(prevState, formData);
+  }
+  return signup(prevState, formData);
+}
+
+export async function logout() {
+  await destroySession();
+  redirect("/");
+}
